17767b23ea1ea3f38a4c267fe31452089d2b1178
Alex A. Mihaylov <minimumlaw@rambler.ru>
2024-02-21 19:46:09 +0300

    Gvardman: autostart MDA software

--- board/gvardman/orangepi3-lts/overlay/usr/local/bin/gvardman-start.sh
+++ board/gvardman/orangepi3-lts/overlay/usr/local/bin/gvardman-start.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 ##############################################################################
 ##############################################################################
+###                                                                        ###
+###                        Let's start MDA software                        ###
+###                                                                        ###
+##############################################################################
 ##############################################################################
 
-echo '############################################'
-echo '# replace /usr/local/bin/gvardman-start.sh #'
-echo '# in  board/gvardman/orengepi3_lts/overlay #'
-echo '# buildroot external  directory to startup #'
-echo '#          script  for  you  code          #'
-echo '############################################'
-
-# Just run "yes" as daemon
-/usr/bin/yes > /dev/null&
+# first - kill all already runing copy
+killall mda 
+# next - start new copy of software
+/usr/local/bin/mda 2>&1 >/dev/null&
