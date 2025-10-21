299606d7530002632c7e582e5e869c9d17f98d77
Alex A. Mihaylov <minimumlaw@rambler.ru>
2025-10-21 19:12:16 +0300

    OPi3 LTS: Sapus: Try touchscreen fixup
    	[!] - revert framebuffer config in extlinux.conf
    	[+] - Fix mda-start.sh for touchscreen calibration and
    		place calibration file on params partition
    	[+] - Add /etc/ts.conf to board overlay

--- board/gvardman/orangepi3-lts/overlay/usr/local/bin/mda-start.sh
+++ board/gvardman/orangepi3-lts/overlay/usr/local/bin/mda-start.sh
@@ -9,12 +9,22 @@
 
 # export path with scripts
 export PATH='/usr/local/bin':$PATH
+export TSLIB_TSDEVICE=/dev/input/event1
+export TSLIB_CALIBFILE=/mnt/touchscreen.calibration
 
 # restore alsa mixer settings
 /usr/sbin/alsactl -U restore
 
 # first - kill all already runing copy
-killall mda 
+killall mda
+
+if [ -f $TSLIB_CALIBFILE ]; then
+    echo Use savved touchscreen calibration
+else
+    /usr/local/bin/remount_storage_read_write.sh
+    ts_calibrate
+    /usr/local/bin/remount_storage_read_only.sh
+fi
 
 # next - start new copy of software
 /usr/local/bin/mda 2>&1 >/dev/null&
