ef51249285d0c1e6f58b40e05bd9cb84d42b4b2e
Alex A. Mihaylov <minimumlaw@rambler.ru>
2026-03-05 20:00:11 +0300

    MDA: QT5: Fix touchscreen/tslib troubles

--- board/gvardman/orangepi3-lts/overlay/usr/local/bin/mda-start.sh
+++ board/gvardman/orangepi3-lts/overlay/usr/local/bin/mda-start.sh
@@ -8,17 +8,17 @@
 ##############################################################################
 
 # export path with scripts
+export LANG=C.UTF-8
 export PATH='/usr/local/bin':$PATH
-export TSLIB_TSEVENTTYPE=INPUT
-export TSLIB_FBDEVICE=/dev/fb0
+export QT_QPA_EGLFS_KMS_CONFIG=/usr/local/bin/kms.json
 export TSLIB_TSDEVICE=/dev/input/event1
 export TSLIB_CALIBFILE=/mnt/touchscreen.calibration
-export TSLIB_CONFFILE=/etc/ts.conf
-export TSLIB_PLUGINDIR=/usr/lib/ts
-# export QT_QPA_PLATFORM=linuxfb
-export QT_QPA_GENERIC_PLUGINS=tslib:/dev/input/event1
 export QT_QPA_EGLFS_NO_LIBINPUT=1
 export QT_QPA_EGLFS_TSLIB=1
+export QT_QPA_GENERIC_PLUGINS=tslib
+export QT_QPA_EVDEV_TOUCHSCREEN_PARAMETERS="tslib"
+export QT_QPA_EGLFS_HIDECURSOR=1
+# export QT_QPA_PLATFORM=linuxfb
 
 # restore alsa mixer settings
 /usr/sbin/alsactl -U restore
